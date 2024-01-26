97cd73f23f2258a608bc9cb90ace1bcb9e482ab1
adieos <athanokwibisono@gmail.com>
2024-01-27 06:56:45 +0700

    feat: add model

--- main.go
+++ main.go
@@ -2,28 +2,23 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/adieos/go-fiber-postgres/model"
+	"github.com/adieos/go-fiber-postgres/setup"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 	"gorm.io/gorm"
 )
 
 type Book struct {
-	author string
-	title string
+	author    string
+	title     string
 	publisher string
 }
 
 type Repository struct {
 	DB *gorm.DB
-}
-
-func (r Repository) SetupRoutes(app *fiber.App) {
-	api := app.Group("/api")
-	// these paths dont apply RESTful principles
-	api.Post("/create-book", r.CreateBook)
-	api.Delete("/delete-book/:id", r.DeleteBook)
-	api.Get("/get-books/:id", r.GetBookById)
-	api.Get("/books", r.GetBooks)
 }
 
 func main() {
@@ -32,15 +27,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	db, err := storage.NewConnection(config)
+	// config
+	config := &setup.Config{
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+		DBName:   os.Getenv("DB_NAME"),
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		SSLMode:  os.Getenv("DB_SSLMODE"),
+	}
+
+	// connect and migrate db
+	db, err := setup.NewConnection(config)
 	if err != nil {
 		log.Fatal("could not load database")
+	}
+	err := model.MigrateDB(db)
+	if err != nil {
+		log.Fatal("could not migrate database")
 	}
 
 	r := Repository{db}
 
 	app := fiber.New()
-	r.SetupRoutes(app)
 	app.Listen(":8080")
 
 }
