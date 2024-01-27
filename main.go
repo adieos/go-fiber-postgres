7e8f1ae7662dea0749f130f10c9cbb5d523c6192
adieos <athanokwibisono@gmail.com>
2024-01-27 20:07:33 +0700

    feat: add handlers for CRUD

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 
-// TODO: refactor it? idk :(
+// TODO: UPDATE LISTENER AND REFACTOR IF POSSIBLE !!!
 
 import (
 	"log"
@@ -45,20 +45,132 @@ func (r *Repository) CreateBookHandler(ctx *fiber.Ctx) error {
 
 	ctx.Status(http.StatusOK).JSON(
 		fiber.Map{"message": "Book created successfully"})
+	log.Println("Sukses nambah")
 	return nil
 }
 
-// func (r *Repository) PutBookListener(ctx *fiber.Ctx) error{
+func (r *Repository) ModifyBookHandler(ctx *fiber.Ctx) error {
+	bookModel := model.Books{}
+	book := Book{}
+	id := ctx.Params("id")
+	err := ctx.BodyParser(&book)
+	if err != nil {
+		ctx.Status(http.StatusUnprocessableEntity).JSON(
+			fiber.Map{"message": "Unable to process response"})
+		log.Println("Error 422: ", err)
+		return err
+	}
 
-// }
+	result := r.db.Model(&bookModel).Where("id = ?", id).Updates(book)
+	if result.Error != nil {
+		ctx.Status(http.StatusInternalServerError).JSON(
+			fiber.Map{"message": "Unable to update book"})
+		log.Println("Error 500: ", result.Error)
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		ctx.Status(http.StatusNotFound).JSON(
+			fiber.Map{"message": "Unable to update book: ID not found"})
+		log.Println("Error 404: id gaada")
+		return nil
+	}
+
+	ctx.Status(http.StatusOK).JSON(
+		fiber.Map{"message": "book edited successfully"})
+	log.Println("Sukses edit")
+	return nil
+}
+
+func (r *Repository) DeleteBookHandler(ctx *fiber.Ctx) error {
+	bookModel := model.Books{}
+	id := ctx.Params("id")
+	if id == "" {
+		ctx.Status(http.StatusBadRequest).JSON(
+			fiber.Map{"message": "Unable to delete book: ID cannot be empty"})
+		log.Println("Error 400: id kosong")
+		return nil
+	}
+
+	result := r.db.Delete(bookModel, id)
+	if result.Error != nil {
+		ctx.Status(http.StatusBadRequest).JSON(
+			fiber.Map{"message": "Unable to delete book"})
+		log.Println("Error 400: ", result.Error)
+		return result.Error
+	}
+	if result.RowsAffected == 0 {
+		ctx.Status(http.StatusNotFound).JSON(
+			fiber.Map{"message": "Unable to delete book: ID not found"})
+		log.Println("Error 404: ID ga ada")
+		return nil
+	}
+
+	ctx.Status(http.StatusOK).JSON(
+		fiber.Map{"message": "Book deleted successfully"})
+	log.Println("Sukses hapus")
+	return nil
+
+}
+
+func (r *Repository) GetAllBooksHandler(ctx *fiber.Ctx) error {
+	books := []model.Books{}
+
+	err := r.db.Find(&books).Error
+	if err != nil {
+		ctx.Status(http.StatusBadRequest).JSON(
+			fiber.Map{"message": "Unable to fetch books"})
+		log.Println("Error 400: ", err)
+		return err
+	}
+
+	ctx.Status(http.StatusOK).JSON(fiber.Map{
+		"message": "Books fetched successfully",
+		"data":    books,
+	})
+	log.Println("Sukses get all")
+	return nil
+
+}
+
+func (r *Repository) GetBookByIDHandler(ctx *fiber.Ctx) error {
+	book := model.Books{}
+	id := ctx.Params("id")
+	if id == "" {
+		ctx.Status(http.StatusBadRequest).JSON(
+			fiber.Map{"message": "ID cannot be empty"})
+		log.Println("Error 400: ID kosong")
+		return nil
+	}
+
+	err := r.db.Where("id = ?", id).First(&book).Error
+	if err != nil {
+		if err == gorm.ErrRecordNotFound {
+			ctx.Status(http.StatusNotFound).JSON(
+				fiber.Map{"message": "Unable to fetch book: ID not found"})
+			log.Println("Error 404: ID gaada")
+			return err
+		}
+		ctx.Status(http.StatusBadRequest).JSON(
+			fiber.Map{"message": "Unable to fetch book"})
+		log.Println("Error 400: ", err)
+		return err
+	}
+
+	ctx.Status(http.StatusOK).JSON(fiber.Map{
+		"message": "Book fetched successfully",
+		"data":    book,
+	})
+	log.Println("Sukses ambil buku")
+	return nil
+}
 
 func (r *Repository) SetRoutes(app *fiber.App) {
 	api := app.Group("/api")
 	api.Post("/books", r.CreateBookHandler)
-	// api.Put("/books/:id", r.PutBookListener)
-	// api.Delete("/books/:id", r.DeleteBookListener)
-	// api.Get("/books", r.GetAllBooksListener)
-	// api.Get("/books/:id", r.GetBookByIDListener)
+	api.Put("/books/:id", r.ModifyBookHandler)
+	api.Delete("/books/:id", r.DeleteBookHandler)
+	api.Get("/books", r.GetAllBooksHandler)
+	api.Get("/books/:id", r.GetBookByIDHandler)
 }
 
 func main() {
